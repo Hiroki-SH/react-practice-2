8065558271169585453ff0aebe76354ac8de7ae7
Hiroki-SH <srymc75@gmail.com>
2022-01-22 19:28:33 +0900

    stateとは

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import { ColoredMessage } from "./components/ColoredMessage";
 
 export const App = () => {
+  // Stateの定義
+  const [num, setNum] = useState(0);
+
+  // ボタンを押した時にStateをカウントアップ
   const onClickButton = () => {
-    alert();
+    setNum(num + 1);
   };
 
   return (
@@ -14,6 +19,7 @@ export const App = () => {
       <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
       <ColoredMessage color="pink">元気です!</ColoredMessage>
       <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
     </>
   );
 };
