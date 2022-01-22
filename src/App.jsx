3439287f134fdded60219ddeb33f3eaec7996bcf
Hiroki-SH <srymc75@gmail.com>
2022-01-22 18:17:07 +0900

    イベントの扱い方

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 export const App = () => {
-  // return null;
+  const onClickButton = () => {
+    alert();
+  };
   return (
     <>
       <h1>こんにちは!</h1>
       <p>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
